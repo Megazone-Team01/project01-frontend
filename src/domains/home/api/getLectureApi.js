56c46545adfc6a5266c1c5881a6c7f2cd4287d55
waney <ssdii66@gmail.com>
2025-12-09 15:16:05 +0900

    [#21]feat:env 추가

--- src/domains/home/api/getLectureApi.js
+++ src/domains/home/api/getLectureApi.js
@@ -1,6 +1,6 @@
 
 export async function getLectureApi(activeTab) {
-    const response = await fetch(`http://localhost:8080/api/home?type=${activeTab}`);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/home?type=${activeTab}`);
     if(!response.ok) throw new Error("데이터 요청을 실패했습니다.");
     return response.json();
 }
