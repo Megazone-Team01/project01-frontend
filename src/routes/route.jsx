fa676c509571b235f1fb01388469e2bfd9726ecf
hye-inA <hye.ina0130@gmail.com>
2025-12-11 20:44:14 +0900

    feat : 라우터 등록

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,12 +1,16 @@
-import {Routes, Route} from "react-router";
+import { Routes, Route } from "react-router";
 import MainLayout from "@/components/layout/MainLayout.jsx";
 import Home from "@/domains/home/page/Home.jsx";
+import MyReservation from "@/domains/reservation/page/MyReservation.jsx";
 
-function CommonRouter(){
-    return  (
+function CommonRouter() {
+
+
+    return (
         <Routes>
-            <Route element={<MainLayout/>}>
-                <Route path="/" element={<Home />}/>
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/my/reservations" element={<MyReservation />} />
             </Route>
         </Routes>
     )
