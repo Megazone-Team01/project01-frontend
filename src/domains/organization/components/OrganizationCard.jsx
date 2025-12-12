aff56267e4ffa8a5c1ff798c42f3fa5dfe0b75f3
seongje00416 <seongje00416@gmail.com>
2025-12-12 09:23:56 +0900

    feat: Organization에 추가된 필드에 따른 화면 추가

--- src/domains/organization/components/OrganizationCard.jsx
+++ src/domains/organization/components/OrganizationCard.jsx
@@ -8,24 +8,21 @@ export const OrganizationCard = ( { organization, onClick } ) => {
         >
             <div className="w-1/2 flex justify-center items-center">
                 { /* 아카데미를 대표하는 사진을 저장하는 곳이 없음. 추후 추가되면 수정 필요 */}
-                <img className="w-full h-full object-cover" src={"./test.jpeg"} alt={"test Image"}/>
+                <img className="w-full h-full object-cover" src={"./test.jpeg"} alt={ organization.url }/>
             </div>
             <div className="w-1/2 h-full p-1 flex flex-col justify-center items-center">
                 <p className="flex-1 flex justify-center items-center text-base font-bold" id="name">
-                    { organization.name }
+                    {organization.name}
                 </p>
-                <p className="flex-1 flex justify-center items-center text-gray-500" id="type">
-                    { /* 이름 다음으로 온라인/오프라인 여부를 알려주고 그 다음으로 온라인이면 홈페이지 주소를, */}
-                    { /* 오프라인이면 아카데미 주소를 보여주고 싶은데 온오프라인 여부를 나타내는 칼럼이 없음 */}
-                    { /* 임의로 addressCode를 넣어둠 */}
-                    { organization.addressCode }
+                <p className="flex-1 text-sm flex justify-center items-center text-gray-500" id="isOnline">
+                    {organization.isOnline}
+                </p>
+                <p className="flex-1 text-sm flex justify-center items-center text-gray-500" id="url">
+                    {organization.url}
                 </p>
                 { /* Organization에 대한 description이 없음. 추가되면 수정 예정 */}
-                <p className="flex-4 flex justify-center items-start text-sm overflow-hidden" id="description">
-                    이 부분은 설명에 대한 부분으로 Text 타입이 올 예정이며 내용이 길어졌을 때 어떻게 되는지 확인이
-                    필요한 부분입니다. 일반적으로 가로 길이는 초과하는 경우 아래로 자동 줄바꿈을 하게 되는데 문제는
-                    이 길이가 세로 길이도 초과하는 경우에는 어떻게 되는지 입니다. 무한히 올라가면 안되니 이름과
-                    온라인 여부 위치를 고정시켜 볼까요?
+                <p className="flex-5 flex justify-center items-start text-sm overflow-hidden" id="description">
+                    { organization.description }
                 </p>
             </div>
         </Card>
--- src/domains/organization/page/UserOrganizationDetailPage.jsx
+++ src/domains/organization/page/UserOrganizationDetailPage.jsx
@@ -1,10 +1,33 @@
 import {useParams} from "react-router";
 import {Separator} from "../../../components/ui/separator.js";
 import {Table, TableBody, TableCell, TableRow} from "../../../components/ui/table.js";
+import {useEffect, useState} from "react";
+import {getOrganization} from "../api/organizationApi.js";
 
 
 export const UserOrganizationDetailPage = () => {
     const { id } = useParams();
+    const [ organization, setOrganization ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState(null);
+
+    useEffect(() => {
+        const fetchOrganization = async () => {
+            try {
+                setLoading(true);
+                const org = await getOrganization( id );
+                setOrganization( org );
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchOrganization();
+    }, []);
+
+    if (loading) return <div>로딩 중...</div>;
+    if (error) return <div>에러: {error}</div>;
 
     return (
       <div className="flex flex-col w-full">
@@ -13,40 +36,40 @@ export const UserOrganizationDetailPage = () => {
                 <img src={"../org.jpeg"} alt={id} />
               </div>
               <div className="flex flex-1 flex-col justify-start items-center m-2">
-                <h2> 메가존 클라우드 </h2>
+                <h2> { organization.name } </h2>
                 <Separator className="my-4" />
                 <Table>
                     <TableBody>
                         <TableRow>
                             <TableCell className="w-[100px]"> 상호명: </TableCell>
-                            <TableCell> 메가존 클라우드 </TableCell>
+                            <TableCell> { organization.name } </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell className="w-[100px]"> 강의 유형: </TableCell>
-                            <TableCell> 온라인 / 오프라인 </TableCell>
+                            <TableCell> { organization.isOnline } </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell className="w-[100px]"> 주소: </TableCell>
-                            <TableCell> 서울특별시 서초구 역삼동 와이즈 플레이즈 2F </TableCell>
+                            <TableCell> { organization.addressCode + " " + organization.addressDetail } </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell className="w-[100px]"> 홈페이지: </TableCell>
-                            <TableCell> www.magazone.com </TableCell>
+                            <TableCell> { organization.url } </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell className="w-[100px]"> 카테고리: </TableCell>
-                            <TableCell> IT > 프로그래밍 </TableCell>
+                            <TableCell> { organization.category } </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell className="w-[100px]"> 강의 수: </TableCell>
-                            <TableCell> 12  </TableCell>
+                            <TableCell> { organization.tel }  </TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
               </div>
           </div>
           <div className="flex w-full min-h-96 bg-red-100">
-
+              { organization.description }
           </div>
       </div>
     );
