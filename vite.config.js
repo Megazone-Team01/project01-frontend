f73949365dda32ee8bb6b113761f5be26bfd0a24
hye-inA <hye.ina0130@gmail.com>
2025-12-16 09:28:06 +0900

    config : vite proxy 설정 추가
    This reverts commit 6284ade69d618cb7d2a67d3c1773192e6d792e13.

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  }
 })
