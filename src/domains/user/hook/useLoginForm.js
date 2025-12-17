b70bc6a168e0db0d2c0cba47890ac67d3e352e95
mindun <pmk10077@gmail.com>
2025-12-17 09:29:43 +0900

    refactor : react-router-dom을 react-router로 변경

--- src/domains/user/hook/useLoginForm.js
+++ src/domains/user/hook/useLoginForm.js
@@ -1,6 +1,5 @@
-
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { login } from "../api/login";
 
 export default function useLoginForm() {
