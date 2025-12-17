eb5a7f657568dedf4e3f2845f2e9f33ba45cc07c
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-12-17 16:06:11 +0900

    refator : authSlice 관련 위치 재수정 (#29)

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -1,10 +1,12 @@
 // store/index.js
 import { configureStore } from '@reduxjs/toolkit';
 import lectureReducer from './lecture/lectureStore.js';
+import authReducer from './auth/authSlice.js';
 
 export const store = configureStore({
     reducer: {
         lecture: lectureReducer,
+        auth: authReducer,
         // 다른 slice들...
     },
 });
