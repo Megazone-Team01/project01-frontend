7442540b386231eb057c6eba8e766652774af013
mindun <pmk10077@gmail.com>
2025-12-17 15:50:22 +0900

    fix : test

--- src/common/store/auth/authSlice.js
+++ src/common/store/auth/authSlice.js
@@ -1,10 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "@/auth/authSlice";
+import { createSlice } from "@reduxjs/toolkit";
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
+const initialState = {
+    isAuthenticated: !!localStorage.getItem("accessToken"),
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    accessToken: localStorage.getItem("accessToken") || null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        loginSuccess: (state, action) => {
+            state.isAuthenticated = true;
+            state.user = action.payload.user;
+            state.accessToken = action.payload.accessToken;
+
+            localStorage.setItem("accessToken", action.payload.accessToken);
+            localStorage.setItem("refreshToken", action.payload.refreshToken);
+            localStorage.setItem("user", JSON.stringify(action.payload.user));
+        },
+        logout: (state) => {
+            state.isAuthenticated = false;
+            state.user = null;
+            state.accessToken = null;
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+        },
+        refreshAccessToken: (state, action) => {
+            state.accessToken = action.payload.accessToken;
+        },
     },
 });
 
-export default store;
+export const { loginSuccess, logout, refreshAccessToken } = authSlice.actions;
+export default authSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -16,11 +16,7 @@ createRoot(document.getElementById('root')).render(
             </BrowserRouter>
         </QueryClientProvider>
     </Provider>
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-    </QueryClientProvider>
 )
+
+
+
