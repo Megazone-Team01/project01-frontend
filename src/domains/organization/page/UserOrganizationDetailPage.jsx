f6fca13260c880566a44fa391a7de1bc62404d9b
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-12-19 10:24:22 +0900

    [#13] WIL: 기관 상세 조회 및 목록 페이지 작업 중 <시연을 위한 PR> (#42)
    * feat: Organization 목록 조회 페이지 UI 제작
    
    * feat: Organization 목록 조회 기능 제작
    
    * feat: Organization 목록 조회 및 상세 조회 페이지 제작 및 연결
    
    * feat: Organization에 추가된 필드에 따른 화면 추가
    
    * WIL: 조직 상세 화면 작업중

--- src/domains/organization/page/UserOrganizationDetailPage.jsx
+++ src/domains/organization/page/UserOrganizationDetailPage.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router";
-import { Separator } from "../../../components/ui/separator.js";
-import { Table, TableBody, TableCell, TableRow } from "../../../components/ui/table.js";
-import { useEffect, useState } from "react";
-import { getOrganization } from "../api/organizationApi.js";
-import OrganizationServiceSection from "../components/OrganizationServiceSection";
+import {useParams} from "react-router";
+import {Separator} from "@/components/ui/separator.js";
+import {Table, TableBody, TableCell, TableRow} from "@/components/ui/table.js";
+import {useEffect, useState} from "react";
+import {getOrganization} from "../api/organizationApi.js";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs.js";
+import {Label} from "@/components/ui/label.js";
+import {Card, CardContent, CardHeader} from "@/components/ui/card.js";
 
 
 export const UserOrganizationDetailPage = () => {
@@ -31,49 +33,95 @@ export const UserOrganizationDetailPage = () => {
     if (error) return <div>에러: {error}</div>;
 
     return (
-        <div className="flex flex-col w-full">
-            <div className="flex w-full min-h-80">
-                <div className="flex flex-1 justify-center items-center m-2">
-                    <img src={"../org.jpeg"} alt={id} />
-                </div>
-                <div className="flex flex-1 flex-col justify-start items-center m-2">
-                    <h2> {organization.name} </h2>
-                    <Separator className="my-4" />
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 상호명: </TableCell>
-                                <TableCell> {organization.name} </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 강의 유형: </TableCell>
-                                <TableCell> {organization.isOnline} </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 주소: </TableCell>
-                                <TableCell> {organization.addressCode + " " + organization.addressDetail} </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 홈페이지: </TableCell>
-                                <TableCell> {organization.url} </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 카테고리: </TableCell>
-                                <TableCell> {organization.category} </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="w-[100px]"> 강의 수: </TableCell>
-                                <TableCell> {organization.tel}  </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-            <div className="flex w-full min-h-96 bg-red-100">
-                {organization.description}
-            </div>
-            <OrganizationServiceSection organizationId={Number(id)} />
-        </div>
+      <div className="flex flex-col w-full">
+          <div className="flex w-full min-h-80">
+              <div className="flex w-full flex-1 justify-center items-center m-2">
+                  {
+                      organization.leadImage ?
+                          <img
+                              className="max-w-full max-h-72 aspect-ratio"
+                              src={`/${organization.leadImage}`} alt={organization.leadImage}/>
+                          :
+                          <div className="bg-gray-50 w-full h-72" />
+                  }
+              </div>
+              <div className="flex flex-1 flex-col justify-start items-center m-2">
+                <h2> { organization.name } </h2>
+                <Separator className="my-4" />
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 상호명: </TableCell>
+                            <TableCell> { organization.name } </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 강의 유형: </TableCell>
+                            <TableCell> { organization.isOnline === 1 ? "온라인" : organization.isOnline === 2 ? "오프라인" : "온라인 + 오프라인" } </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 주소: </TableCell>
+                            <TableCell> { organization.addressCode + " " + organization.addressDetail } </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 홈페이지: </TableCell>
+                            <TableCell> { organization.webpage } </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 전화번호: </TableCell>
+                            <TableCell> { organization.tel } </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="w-[100px]"> 소개: </TableCell>
+                            <TableCell> { organization.description }  </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+              </div>
+          </div>
+          <Separator className="my-1" />
+          <div className="flex flex-col w-full min-h-96 pt-2">
+              <Tabs defaultValue="lectures">
+                  <div className="flex flex-1 flex-col justify-start items-center m-2">
+                      <TabsList className="w-2/3 bg-gray-200 border">
+                          <TabsTrigger className="w-1/2" value="introduction"> 소개 </TabsTrigger>
+                          <TabsTrigger className="w-1/2" value="lectures"> 강의 </TabsTrigger>
+                          <TabsTrigger className="w-1/2" value="teachers"> 강사 </TabsTrigger>
+                      </TabsList>
+                  </div>
+                  <TabsContent value="introduction">
+                      <div className="w-full bg-red-100 h-[50vh]" />
+                  </TabsContent>
+                  <TabsContent value="lectures">
+                      <h1 className="font-bold text-xl text-center py-3"> 강의 목록 </h1>
+                      <div className="grid grid-cols-3 gap-4">
+                          <Card className="bg-neutral-50 hover:bg-neutral-100 hover:cursor-pointer">
+                              <div className="w-full h-32 bg-red-100">
+
+                              </div>
+                              <div className="w-full h-16 bg-blue-100">
+
+                              </div>
+                          </Card>
+                          <Card>
+                              dfdf
+                          </Card>
+                          <Card>
+                              dfdf
+                          </Card>
+                          <Card>
+                              dfdf
+                          </Card>
+                          <Card>
+                              dfdf
+                          </Card>
+                      </div>
+                  </TabsContent>
+                  <TabsContent value="teachers">
+                    
+                  </TabsContent>
+              </Tabs>
+          </div>
+      </div>
     );
 
 }
