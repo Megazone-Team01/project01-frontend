0dbf2eca172c6ddd40fa9149a2579a8e8d32ae23
hye-inA <hye.ina0130@gmail.com>
2025-12-20 14:40:58 +0900

    feat : 강사 상세 내용 api 추가

--- src/domains/meeting/api/meetingApi.js
+++ src/domains/meeting/api/meetingApi.js
@@ -8,6 +8,11 @@ export const getTeachersByOrganization = async (organizationId) => {
     return response.data;
 };
 
+export const getTeacherDetail = async (teacherId) => {
+    const response = await axiosInstance.get(`/v1/meetings/teachers/${teacherId}`);
+    return response.data;
+};
+
 export const getMyMeetings = async (type = 'ALL', status = null) => {
     const response = await axiosInstance.get('/v1/meetings/my', {
         params: { type, status }
