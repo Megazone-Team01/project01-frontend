f99f661f1f8a9c62d7b760f896b66bba64bb8627
waney <ssdii66@gmail.com>
2025-12-23 15:15:22 +0900

    [#67]fix: lecture apply button 취소, 신청 에러 수정

--- src/domains/lecture/components/common/AlertBox.jsx
+++ src/domains/lecture/components/common/AlertBox.jsx
@@ -28,7 +28,6 @@ export function AlertBox({ text, startAt, endAt, lectureType, lectureId}) {
             }
         });
     }
-
     const handleCancel = () => {
         cancel.mutate({
             lectureType: lectureType,
