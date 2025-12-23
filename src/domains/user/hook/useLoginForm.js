a888d8bdc04372f893d4e973f921252a1bc6de54
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-12-23 11:54:45 +0900

    refactor : 리덕스에 타입추가 (#69)

--- src/domains/user/hook/useLoginForm.js
+++ src/domains/user/hook/useLoginForm.js
@@ -66,6 +66,7 @@ export default function useLoginForm() {
                             name: result.name,
                             email: result.email,
                             role: result.role,
+                            type: result.type,
                         },
                         accessToken: result.accessToken,
                     })
@@ -76,7 +77,7 @@ export default function useLoginForm() {
                 alert(`로그인 성공! 환영합니다, ${result.name}`);
 
                 console.log("로그인 성공! 액세스토큰:", result.accessToken);
-                console.log("유저 정보:", { id: result.id, email: result.email, name: result.name, role: result.role });
+                console.log("유저 정보:", { id: result.id, email: result.email, name: result.name, role: result.role, type: result.type});
 
                 navigate("/");
 
