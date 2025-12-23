ab322f9a43fefed21e8a7462b993ba0dc1fa5f81
mindun <pmk10077@gmail.com>
2025-12-23 20:30:03 +0900

    refactor : 풋터 수정

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,11 +4,11 @@ function Footer() {
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center gap-12 px-4 md:px-8">
 
                 {/* 좌측: 브랜드 + 슬로건 */}
-                <div className="flex flex-col md:flex-1 text-center ">
+                <div className="flex flex-col md:flex-1 ml-4 md:ml-6">
                     <h1 className="text-6xl md:text-7xl font-extrabold tracking-tight drop-shadow-lg
-                                             bg-clip-text text-transparent bg-gradient-to-r from-primary via-white to-primary mb-4 md:mb-6
-                                             [-webkit-text-stroke:0.25px_black]">
-                    LinkEd
+                                   bg-clip-text text-transparent bg-gradient-to-r from-primary via-white to-primary mb-4 md:mb-6
+                                   [-webkit-text-stroke:0.25px_black]">
+                        LinkEd
                     </h1>
                     <p className="text-xl md:text-2xl opacity-80 mb-4">
                         Learn. Connect. Grow.
@@ -22,13 +22,12 @@ function Footer() {
                 {/* 우측: 회사 정보 (좌측 정렬) */}
                 <div className="flex flex-col md:flex-1 gap-4 text-sm md:text-base opacity-90">
                     <InfoRow label="사업명" value="LinkEd" />
+                    <InfoRow label="대표자" value="박민경, 임창완, 임성제, 허혜인" />
                     <InfoRow label="전화번호" value="010-1234-5678" />
                     <InfoRow label="이메일" value="contact@linked.com" />
                     <InfoRow label="사업자등록번호" value="123-45-67890" />
                     <InfoRow label="위치" value="서울특별시 강남구 테헤란로 123" />
                     <InfoRow label="고객센터 운영" value="월~금 09:00 - 18:00" />
-                    <InfoRow label="만든이" value="박민경, 임창완, 임성제, 허혜인" />
-
                 </div>
             </div>
 
